a5b467ef65e7f2efd26f98b98d943fa74c026c0f
Anniek van Dijk <anniek.vandijk@duo.nl>
2016-06-16 12:11:07 +0200

    cucumberIT

---
+++ src/it/java/com/github/anniekvandijk/mrrs/it/RunCucumberIT.java
@@ -0,0 +1,18 @@
+package com.github.anniekvandijk.mrrs.it;
+
+import org.junit.runner.RunWith;
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+/**
+ * Created by Anniek van Dijk on 16-6-2016.
+ */
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = { "html:target/cucumber/report.html", "json:target/cucumber-report.json" },
+        features = { "src/it/resources/features" },
+        tags = { "@run"}
+)
+public class RunCucumberIT {
+    //
+}
