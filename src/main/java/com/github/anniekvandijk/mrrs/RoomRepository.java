c19695af0b10db2cf92b82c597e7d8096c131466
Anniek van Dijk <anniek.vandijk@duo.nl>
2016-06-28 14:22:00 +0200

    RoomRepository

---
+++ src/main/java/com/github/anniekvandijk/mrrs/RoomRepository.java
@@ -0,0 +1,36 @@
+package com.github.anniekvandijk.mrrs;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RoomRepository {
+    private Map<String, MeetingRoom> roomsByLocation = new HashMap<>();
+    private Map<String, MeetingRoom> roomsByName = new HashMap<>();
+
+
+    public void add(MeetingRoom meetingRoom) {
+        if (roomsByLocation.containsKey(meetingRoom.getLocation())) {
+            throw new IllegalArgumentException("Argument 'location' with value '" + meetingRoom.getLocation()
+                    + "' is an already known MeetingRoom");
+        }
+        roomsByLocation.put(meetingRoom.getLocation(), meetingRoom);
+
+        if (roomsByName.containsKey(meetingRoom.getName())) {
+            throw new IllegalArgumentException("Argument 'name' with value '" + meetingRoom.getName()
+                    + "' is an already known MeetingRoom");
+        }
+        roomsByName.put(meetingRoom.getName(), meetingRoom);
+    }
+
+    public MeetingRoom search(String text) {
+        if ((! roomsByLocation.containsKey(text)) && (! roomsByName.containsKey(text))) {
+            throw new IllegalArgumentException("Argument 'text' with value '" + text
+                    + "' is not a known MeetingRoom");
+        }
+
+        if (roomsByLocation.containsKey(text)) {
+            return roomsByLocation.get(text);
+        }
+        return roomsByName.get(text);
+    }
+}
---
+++ src/test/java/com/github/anniekvandijk/mrrs/RoomRepositoryTest.java
@@ -0,0 +1,96 @@
+package com.github.anniekvandijk.mrrs;
+
+import org.hamcrest.Matcher;
+import org.hamcrest.core.IsEqual;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import java.util.regex.Pattern;
+
+import static org.junit.Assert.assertEquals;
+
+public class RoomRepositoryTest {
+
+    String name;
+    String location;
+    int capacity;
+
+    @Rule
+    public ExpectedException exception = ExpectedException.none();
+
+    @Test
+    public void addMeetingRoom() {
+
+        name = "Meetingroom 1";
+        location = "Z1507";
+        capacity = 2;
+        MeetingRoom meetingRoom = new MeetingRoom(name, location, capacity);
+
+        RoomRepository roomRepository = new RoomRepository();
+
+        roomRepository.add(meetingRoom);
+
+        String zoekLocation = "Z1507";
+        assertEquals(meetingRoom, roomRepository.search(zoekLocation));
+    }
+
+    @Test
+    public void addDoubleMeetingRoom() {
+
+        exception.expect(IllegalArgumentException.class);
+        exception.expectMessage(new IsEqual<String>("Argument 'location' with value 'Z1510' is an already known MeetingRoom"));
+
+        RoomRepository roomRepository = new RoomRepository();
+
+        name = "Meetingroom 1";
+        location = "Z1510";
+        capacity = 2;
+        MeetingRoom meetingRoom1 = new MeetingRoom(name, location, capacity);
+        roomRepository.add(meetingRoom1);
+
+        name = "Meetingroom 2";
+        location = "Z1510";
+        capacity = 4;
+        MeetingRoom meetingRoom2 = new MeetingRoom(name, location, capacity);
+        roomRepository.add(meetingRoom2);
+    }
+
+    @Test
+    public void SearchNonExistingMeetingRoom() {
+
+        exception.expect(IllegalArgumentException.class);
+        exception.expectMessage(new IsEqual<String>("Argument 'text' with value 'Z1506' is not a known MeetingRoom"));
+
+        name = "Meetingroom 1";
+        location = "Z1507";
+        capacity = 2;
+        MeetingRoom meetingRoom = new MeetingRoom(name, location, capacity);
+
+        RoomRepository roomRepository = new RoomRepository();
+
+        roomRepository.add(meetingRoom);
+
+        roomRepository.search("Z1506");
+    }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void SearchNonExistingMeetingRoom2() {
+        name = "Meetingroom 1";
+        location = "Z1507";
+        capacity = 2;
+        MeetingRoom meetingRoom = new MeetingRoom(name, location, capacity);
+
+        RoomRepository roomRepository = new RoomRepository();
+        roomRepository.add(meetingRoom);
+
+        try {
+            roomRepository.search("Z1506");
+        }
+        catch (IllegalArgumentException iae) {
+            assertEquals("Argument 'text' with value 'Z1506' is not a known MeetingRoom", iae.getMessage());
+            throw iae;
+        }
+
+    }
+}
