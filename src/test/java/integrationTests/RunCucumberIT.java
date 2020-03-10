9fd68242084833adf85bef08179e33694eabf1f6
anniekvandijk <anniek@animundo.nl>
2020-03-10 16:36:24 +0100

    report directory unittests and integration tests

--- src/test/java/integrationTests/RunCucumberIT.java
+++ src/test/java/integrationTests/RunCucumberIT.java
@@ -9,7 +9,8 @@ import org.junit.runner.RunWith;
  */
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        plugin = { "json:target/cucumber/cucumber.json" },
+     //   plugin = { "json:target/cucumber/cucumber.json" },
+        plugin = { "pretty", "junit:target/reports/Cucumber.xml" },
         features = "src/test/resources/features",
         strict = true
 )
